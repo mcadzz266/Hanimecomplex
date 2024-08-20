8efe6f0f1851bd12108d0560816c7f9a7416673f
mcadzz266 <141048889+mcadzz266@users.noreply.github.com>
2024-08-20 10:28:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7314936320:AAGkfUxfNI8qhNujAddKohl45oPT3-jIQN4")
-API_ID = int(os.environ.get("API_ID", "20234710"))
-API_HASH = os.environ.get("API_HASH", "abcf167fb12d540b887fe135e83874fa")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5619620434:AAFWHpxWH5p91bouKz0g4-A0ZPih1xoIT8M")
+API_ID = int(os.environ.get("API_ID", "18401114"))
+API_HASH = os.environ.get("API_HASH", "e9105cffc9ef49b4011dfeb843acb091")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "6796791078"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "684727861"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://underrated:underrated@cluster0.9ng6oal.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "filesharexbot")
 
@@ -28,7 +28,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
 try:
-    ADMINS=[7085541484]
+    ADMINS=[684727861]
     for x in (os.environ.get("ADMINS", "6796791078").split()):
         ADMINS.append(int(x))
 except ValueError:
@@ -67,7 +67,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need 
 
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6796791078)
+ADMINS.append(684727861)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 

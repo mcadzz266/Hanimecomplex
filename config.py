0820b4fa2c7d1baf5c07c4b702d381877f9b45ac
mcadzz266 <141048889+mcadzz266@users.noreply.github.com>
2024-08-20 11:01:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DB_NAME = os.environ.get("DB_NAME", "mcadzzseries")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001584064219"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002116090334"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001940056900"))
 
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
